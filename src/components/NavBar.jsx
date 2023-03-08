1ccbdc4266b9485a4f89e0abe40dcf3ce5e299a0
unknown <sharonkessio6@gmail.com>
2023-03-08 18:47:47 +0300

    design navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
+import logo from '../images/logo/logo.png';
 
 function NavBar() {
   const [nav, setNav] = useState(false);
@@ -8,15 +9,16 @@ function NavBar() {
    <div className=''>
     <div className='md:hidden flex justify-between m-2'>
       <div onClick={() => setNav(!nav)}><FaBars size={30} color={'green'} /></div>
-      <div>LOGO</div>
+      <div className='w-10 h-10'><img src={logo} alt="logo-image" class='object-contain' /></div>
     </div>
     <div className='hidden md:block fixed top-0 bottom-0 p-2 w-[200px] overflow-y-auto text-center bg-white shadow h-screen border-r-3 border-gray-400'>
-    <ul className='mt-40 font-montserrat font-bold text-left text-font-color pl-2'>
+    <div className='w-44 pt-8 flex justify-center items-center'><img src={logo} alt="logo-image" class='w-32' /></div>
+    <ul className='mt-30 font-montserrat font-bold text-left text-font-color pl-2'>
       <li className='my-7'><NavLink to="/">HOME</NavLink></li>
-      <li className='my-7'><NavLink to="/reserve">RESERVE BIKE</NavLink></li>
-      <li className='my-7'><NavLink to="/myreservations">MY RESERVATIONS</NavLink></li>
-      <li className='my-7'><NavLink to="/additem">ADD MOTORBIKE</NavLink></li>
-      <li className='my-7'><NavLink to="/deleteitem">DELETE MOTORBIKE</NavLink></li>
+      <li className='my-7'><NavLink to="/reserve">RESERVE</NavLink></li>
+      <li className='my-7'><NavLink to="/myreservations">RESERVATIONS</NavLink></li>
+      <li className='my-7'><NavLink to="/additem">ADD</NavLink></li>
+      <li className='my-7'><NavLink to="/deleteitem">DELETE</NavLink></li>
     </ul>
     </div>
     {nav && (
@@ -24,10 +26,10 @@ function NavBar() {
       <div onClick={() => setNav(!nav)} className='m-4'><FaTimes size={30} color={'green'} /></div>
     <ul className='flex-col font-montserrat font-bold text-font-color'>
       <li onClick={() => setNav(!nav)} className='my-5'><NavLink to="/">HOME</NavLink></li>
-      <li onClick={() => setNav(!nav)} className='my-5'><NavLink to="/reserve">RESERVE BIKE</NavLink></li>
-      <li onClick={() => setNav(!nav)} className='my-5'><NavLink to="/myreservations">MY RESERVATIONS</NavLink></li>
-      <li onClick={() => setNav(!nav)} className='my-5'><NavLink to="/additem">ADD MOTORBIKE</NavLink></li>
-      <li onClick={() => setNav(!nav)} className='my-5'><NavLink to="/deleteitem">DELETE MOTORBIKE</NavLink></li>
+      <li onClick={() => setNav(!nav)} className='my-5'><NavLink to="/reserve">RESERVE</NavLink></li>
+      <li onClick={() => setNav(!nav)} className='my-5'><NavLink to="/myreservations">RESERVATIONS</NavLink></li>
+      <li onClick={() => setNav(!nav)} className='my-5'><NavLink to="/additem">ADD</NavLink></li>
+      <li onClick={() => setNav(!nav)} className='my-5'><NavLink to="/deleteitem">DELETE</NavLink></li>
     </ul>
     </div>
     )}
