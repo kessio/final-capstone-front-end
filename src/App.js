c8e2df0ed92a45915b2687a34508b9a84a40e612
unknown <sharonkessio6@gmail.com>
2023-03-08 12:05:14 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import NavBar from "./components/NavBar";
+import Home from './components/Home';
+import ReserveMotorcycle from './components/ReserveMotorcycle';
+import MyReservations from './components/MyReservations';
+import AddMotorcycle from './components/AddMotorcycle';
+import DeleteMotorcycle from './components/DeleteMotorcycle';
+
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="font-montserrat font-black text-4xl">
-        HELLO WORLD
-      </h1>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/reserve" element={<ReserveMotorcycle />} />
+        <Route path="/myreservations" element={<MyReservations />} />
+        <Route path="/additem" element={<AddMotorcycle />} />
+        <Route path="/deleteitem" element={<DeleteMotorcycle />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h2>Home page is the main page</h2>
+        On the main page, the user can see a list of motorcycles
+    </div>
+  )
+}
+
+export default Home
