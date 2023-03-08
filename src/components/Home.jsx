523f259c7a3fe97387825874cd472637e8e43e57
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-08 18:09:04 +0000

    fix linters errors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div className='lg:ml-40 ml-2'>
-        <h2>Home page is the main page</h2>
+    <div className="lg:ml-40 ml-2">
+      <h2>Home page is the main page</h2>
+      <p>
         On the main page, the user can see a list of motorcycles
+      </p>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
