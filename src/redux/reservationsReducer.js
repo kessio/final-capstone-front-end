95c2c8dcd683debfa095de0fdb9b0b277930d10a
ecekpo <ememekpo@yahoo.com>
2023-03-08 13:27:48 +0100

    create redux store

--- src/redux/reservationsReducer.js
+++ src/redux/reservationsReducer.js
@@ -1,1 +1,16 @@
+const initialState = [];
 
+const reservationsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'FETCH_RESERVATIONS':
+      return action.payload;
+    case 'ADD_RESERVATION':
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
+
+
+
+export default reservationsReducer;
