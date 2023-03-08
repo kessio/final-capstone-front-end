883056704465ae803b4d944c35e0327922920c2c
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-08 18:03:32 +0000

    fix linters error

--- src/components/AddMotorcycle.jsx
+++ src/components/AddMotorcycle.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
 
 function AddMotorcycle() {
   return (
     <div>
-        <h2 className='font-noto text-2xl underline'>AddMotorcycle Page</h2>
-        <p>When the user clicks the "Add item" (AddMotorcycle) link in the navigation panel they can see a form for adding a new item.</p>
-        <p>Make sure that the "Add item" (AddMotorcycle) and "Delete item" (DeleteMotorcycle) links are accessible only by users who are admins.</p>
+      <h2 className="font-noto text-2xl underline">AddMotorcycle Page</h2>
+      <p>
+        When the user clicks the "Add item" (AddMotorcycle) link in the
+        navigation panel they can see a form for adding a new item.
+      </p>
+      <p>
+        Make sure that the "Add item" (AddMotorcycle) and "Delete item" (DeleteMotorcycle)
+        links are accessible only by users who are admins.
+      </p>
     </div>
-  )
+  );
 }
 
-export default AddMotorcycle
+export default AddMotorcycle;
--- src/components/ReserveMotorcycle.jsx
+++ src/components/ReserveMotorcycle.jsx
@@ -1,14 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 
 function ReserveMotorcycle() {
   return (
     <div>
-        <h2>ReserveMotorcycle page</h2>
-        <p>To reserve an appointment, the user has to select a date and city (username and selected item are autofilled).
-           Use the design based on the "Book a vespa test-ride" and add all necessary inputs.
-           The user can also access the "Reserve" page from the navigation panel. In that case only username is autofilled.</p>
+      <h2>ReserveMotorcycle page</h2>
+      <p>
+        To reserve an appointment, the user has to select a date and city
+        (username and selected item are autofilled).Use the design based on the "Book a
+        vespa test-ride" and add all necessary inputs. The user can also access the "Reserve"
+        page from the navigation panel. In that case only username is autofilled.
+      </p>
     </div>
-  )
+  );
 }
 
-export default ReserveMotorcycle
+export default ReserveMotorcycle;
