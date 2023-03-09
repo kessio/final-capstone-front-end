aed9bcc1b08175d599c9e4c2140e5b84fccc8860
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-09 08:34:14 +0000

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import Home from './components/Home';
 import ReserveMotorcycle from './components/ReserveMotorcycle';
 import MyReservations from './components/MyReservations';
 import AddMotorcycle from './components/AddMotorcycle';
+import Signup from './components/user_auth/Signup';
 import DeleteMotorcycle from './components/DeleteMotorcycle';
+import Login from './components/user_auth/Login';
 
 function App() {
   return (
     <div>
       <NavBar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Signup />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/reserve" element={<ReserveMotorcycle />} />
         <Route path="/myreservations" element={<MyReservations />} />
         <Route path="/additem" element={<AddMotorcycle />} />
