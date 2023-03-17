785a5e0e9afeda6007f65a6380e5ee2bbcdeb03d
unknown <sharonkessio6@gmail.com>
2023-03-17 17:02:59 +0300

    erros

--- src/components/AddMotorcycle.jsx
+++ src/components/AddMotorcycle.jsx
@@ -16,10 +16,10 @@ const AddMotorcycle = () => {
     dispatch(addMotorcycle(motorcycle));
   };
 
- /* const checkAuthUser = () => {
+  const checkAuthUser = () => {
     if (!isTokenSet) navigate('/');
   };
-  */
+  
 
   useEffect(() => {
     checkAuthUser();
@@ -33,12 +33,12 @@ const AddMotorcycle = () => {
     event.preventDefault();
     const form = event.target;
     const formData = new FormData();
-    if (form.name.value && form.description.value && form.price.value && form.model.value && form.image.files[0]) {
+    if (form.name.value && form.description.value && form.price.value && form.model.value && form.image.value) {
       formData.append('motorcycle[name]', form.name.value);
       formData.append('motorcycle[description]', form.description.value);
       formData.append('motorcycle[price]', form.price.value);
       formData.append('motorcycle[model]', form.model.value);
-      formData.append('motorcycle[image]', form.image.files[0]);
+      formData.append('motorcycle[image]', form.image.value);
       setFormData(formData);
     }
   };
@@ -73,7 +73,7 @@ const AddMotorcycle = () => {
               <div className="col-span-3 sm:col-span-2">
                 <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-900">Price</label>
                 <div className="mt-2 flex rounded-md shadow-sm">
-                  <input type="text" name="price" id="price" required className="block w-full flex-1 rounded-none rounded-r-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                  <input type="number" name="price" id="price" required className="block w-full flex-1 rounded-none rounded-r-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                 </div>
               </div>
 
@@ -92,23 +92,12 @@ const AddMotorcycle = () => {
               </div>
             </div>
 
-            <div>
-              <label className="block text-sm font-medium leading-6 text-gray-900">Cover photo</label>
-              <div className="mt-2 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                <div className="space-y-1 text-center">
-                  <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                  </svg>
-                  <div className="flex text-sm text-gray-600">
-                    <label htmlFor="image" className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
-                      <span>Upload image</span>
-                      <input id="image" name="image" type="file" accept="image/*" required className="sr-only" />
-                    </label>
-                  </div>
-                  <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+            <div className="col-span-3 sm:col-span-2">
+                <label htmlFor="image" className="block text-sm font-medium leading-6 text-gray-900">Image Url</label>
+                <div className="mt-2 flex rounded-md shadow-sm">
+                  <input type="text" name="image" id="image" required className="block w-full flex-1 rounded-none rounded-r-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                 </div>
               </div>
-            </div>
             
           </div>
           <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
