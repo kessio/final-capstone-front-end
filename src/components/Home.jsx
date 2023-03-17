45893d9f66a877453bf70098a7498ad2609007af
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-17 11:06:40 +0000

    update Home.js

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,42 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { availableMotorcycles } from '../redux/Auth/homeSlice';
 
-function Home() {
+const Home = () => {
+  const bikes = useSelector(availableMotorcycles);
+  // const status = useSelector(allStatus);
+  const navigate = useNavigate();
+
+  console.log(bikes);
+
   return (
-    <div className="lg:ml-40 ml-2">
-      <h2>Home page is the main page</h2>
-      <p>
-        On the main page, the user can see a list of motorcycles
-      </p>
+    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold mb-4">Motorcycle Rentals</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {bikes.map((motorcycle) => (
+          <div
+            key={motorcycle.id}
+            className="bg-white rounded-lg p-4 shadow-md flex flex-col justify-between"
+            onClick={() => navigate(`/motorcycles-details/${motorcycle.id}`)}
+          >
+            <img src={motorcycle.image} />
+            <div>
+              <h2 className="text-2xl font-bold mb-2">{motorcycle.name}</h2>
+              <p className="mb-2">
+                Rate:
+                {motorcycle.model}
+                /hr
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
