382a2ae6d8196e0a4df7471484f8b3ecaadf0721
ecekpo <ememekpo@yahoo.com>
2023-03-17 14:58:24 +0100

    fix linter

--- src/components/ReserveMotorcycle.jsx
+++ src/components/ReserveMotorcycle.jsx
@@ -1,11 +1,62 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-// import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import axios from 'axios';
-// import api from './api';
-// import { addReservation } from '../redux/reservationActions';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import moment from 'moment';
+import {
+  allMessages,
+  reserveMotorcycle,
+  allStatus,
+} from '../redux/reservationSlice';
+import useToken from '../redux/Auth/useToken';
+import { availableMotorcycles, motorcycle } from '../redux/auth/homeSlice';
+import { authenticatedUser } from '../redux/Auth/authSlice';
 
 const ReserveMotorcycle = () => {
-};
+  const [startTime, setStartTime] = useState(null);
+  const [endTime, setEndTime] = useState(null);
 
-export default ReserveMotorcycle;
+  const currentUser = useSelector(authenticatedUser);
+  const message = useSelector(allMessages);
+  const status = useSelector(allStatus);
+  const selectedMotorcycle = useSelector(motorcycle);
+  const [motorcycleId, setMotorcycleId] = useState(0);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isTokenSet = useToken();
+
+  const handleDateFormat = (date) => moment(dayjs(date).toDate()).format('YYYY-MM-DD');
+
+  const handleMotorcycleId = (motorcycleId) => setMotorcycleId(+motorcycleId);
+
+  const handleReserve = () => {
+    const reservation = {
+      start_time: startTime,
+      end_time: endTime,
+      motorcycle_id: motorcycleId,
+    };
+
+    const reservationObject = {
+      reservation,
+      userId: currentUser.id,
+    };
+    dispatch(reserveMotorcycle(reservationObject));
+  };
+
+  const navigateReservation = () => {
+    if (message === 'Motorcycle has been successfully booked') { navigate('/reservation'); }
+  };
+
+  const checkAuthUser = () => {
+    if (!isTokenSet) navigate('/login');
+  };
+
+  const handleSelectedMotorcycle = () => {
+    if (selectedMotorcycle) setMotorcycleId(selectedMotorcycle);
+  };
+
+  useEffect(() => {
+    handleSelectedMotorcycle();
+    navigateReservation();
+    checkAuthUser();
+  }, [message, isTokenSet, selectedMotorcycle]);
+};
