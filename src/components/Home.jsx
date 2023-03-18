61fd2fb1900d8d85c5f7eb19b0a451d43ca371ed
unknown <sharonkessio6@gmail.com>
2023-03-18 10:30:20 +0300

    home cnages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/alt-text */
-import React, {useEffect} from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { selectavailableMotorcycles } from '../redux/Auth/homeSlice';
 ///import { fetchItems } from '../redux/items/displayItemsSlice';
 
 const Home = () => {
-  const dispatch = useDispatch();
+  //const dispatch = useDispatch();
   const bikes = useSelector(selectavailableMotorcycles);
   // const status = useSelector(allStatus);
   const navigate = useNavigate();
@@ -18,7 +18,7 @@ const Home = () => {
     <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-4xl font-bold mb-4">Motorcycle Rentals</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {items.map((motorcycle) => (
+        {bikes.map((motorcycle) => (
           <div
             key={motorcycle.id}
             className="bg-white rounded-lg p-4 shadow-md flex flex-col justify-between"
